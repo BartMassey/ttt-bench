af157505635283278e5c81c816f0320dad39045e
Bart Massey <bart@cs.pdx.edu>
2016-07-22 23:08:00 -0700

    fixed a bunch of Erlang board bugs

--- erlang/board.erl
+++ erlang/board.erl
@@ -8,25 +8,31 @@
 -export([new/0, get/3, set/4]).
 
 % Initialize a row of the board.
-init_row(C, Row) when C < 3 ->
-    array:set(C, 0, Row).
+init_row(C, Row) when C > 2 ->
+    Row;
+
+init_row(C, Row) ->
+    Row1 = array:set(C, 0, Row),
+    init_row(C + 1, Row1).
 
 % Create and initialize the rows of the board.
-build_board(R, Board) when R < 3 ->
+build_board(R, Board) when R > 2 ->
+    Board;
+
+build_board(R, Board) ->
     Row = array:new(3),
-    init_row(0, Row),
-    array:set(R, Row, Board),
-    build_board(R + 1, Board).
+    Row1 = init_row(0, Row),
+    Board1 = array:set(R, Row1, Board),
+    build_board(R + 1, Board1).
 
 new() ->
     Board = array:new(3),
-    build_board(0, Board),
-    Board.
+    build_board(0, Board).
     
 get(R, C, Board) ->
     array:get(C, array:get(R, Board)).
 
-set(R, C, Board, V) ->
+set(R, C, V, Board) ->
     Row = array:get(R, Board),
     Row1 = array:set(C, V, Row),
     array:set(R, Row1, Board).
