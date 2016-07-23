4f6f3545cc976494c0dbc738408caeadb405a772
Bart Massey <bart@cs.pdx.edu>
2016-07-22 23:26:41 -0700

    fixed a bunch of bugs in gamevalue.erl; more still there

--- erlang/gamevalue.erl
+++ erlang/gamevalue.erl
@@ -11,56 +11,63 @@
 has_diagonal(_, _, D) when D > 2 -> true;
 
 has_diagonal(OnMove, Board, D) ->
-    board:get(D, D, Board) == OnMove and
+    board:get(D, D, Board) == OnMove andalso
     has_diagonal(OnMove, Board, D + 1).
     
 % Scan for opposite diagonal.
 has_opposite_diagonal(_, _, D) when D > 2 -> true;
 
 has_opposite_diagonal(OnMove, Board, D) ->
-    board:get(D, 2 - D, Board) == OnMove and
+    board:get(D, 2 - D, Board) == OnMove andalso
     has_opposite_diagonal(OnMove, Board, D + 1).
 
 % Check this row.
 has_row(_, _, _, C) when C > 2 -> true;
 
 has_row(OnMove, Board, R, C) ->
-    board:get(R, C, Board) == OnMove and
+    board:get(R, C, Board) == OnMove andalso
     has_row(OnMove, Board, R, C + 1).
 
 % Scan for rows.
-has_some_row(OnMove, Board, R) when R < 3 ->
-    has_row(OnMove, Board, R, 0);
+has_some_row(_, _, R) when R > 3 ->
+    false;
 
-has_some_row(OnMove, Board, R) when R < 3 ->
+has_some_row(OnMove, Board, R) ->
+    has_row(OnMove, Board, R, 0) orelse
     has_some_row(OnMove, Board, R + 1).
 
 % Check this column.
 has_col(_, _, R, _) when R > 2 -> true;
 
 has_col(OnMove, Board, R, C) ->
-    board:get(R, C, Board) == OnMove and
-    has_row(OnMove, Board, R + 1, C).
+    board:get(R, C, Board) == OnMove andalso
+    has_col(OnMove, Board, R + 1, C).
 
 % Scan for columns.
-has_some_col(OnMove, Board, C) when C < 3 ->
-    has_col(OnMove, Board, C, 0) or
+has_some_col(_, _, C) when C > 3 ->
+    false;
+
+has_some_col(OnMove, Board, C) ->
+    has_col(OnMove, Board, 0, C) orelse
     has_some_col(OnMove, Board, C + 1).
 
 % Succeeds if there is a win on the board for
 % the side on move.
 has_win(Board, OnMove) ->
-    has_diagonal(Board, OnMove, 0) or
-    has_opposite_diagonal(Board, OnMove, 0) or
-    has_some_row(Board, OnMove, 0) or
-    has_some_col(Board, OnMove, 0).
+    has_diagonal(OnMove, Board, 0) orelse
+    has_opposite_diagonal(OnMove, Board, 0) orelse
+    has_some_row(OnMove, Board, 0) orelse
+    has_some_col(OnMove, Board, 0).
 
 % Succeeds if there is a blank on the board.
+has_move(_, R, _) when R > 2 ->
+    false;
+
 has_move(Board, R, C) when C > 2 ->
     has_move(Board, R + 1, 0);
 
-has_move(Board, R, C) when R =< 2 ->
-    board:get(R, C, Board) /= 0 and
+has_move(Board, R, C) ->
+    board:get(R, C, Board) /= 0 andalso
     has_move(Board, R, C + 1).
 
 % Returns the value of the game to the
