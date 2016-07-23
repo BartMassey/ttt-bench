5d06c3a096bd2218b8435a9cdb07579e192ddfb6
Bart Massey <bart@cs.pdx.edu>
2016-07-23 13:52:33 -0700

    added repcount argument to erlang

--- erlang/ttt.erl
+++ erlang/ttt.erl
@@ -5,18 +5,19 @@
 % distribution of this software for license terms.
 
 -module(ttt).
--export([start/0, bench/0]).
+-export([start/0, bench/1]).
 
 start() ->
     GameValue = negamax:negamax(-1, board:new()),
     io:fwrite("~B~n", [GameValue]).
 
-loop(10, GameValue) ->
+loop(0, GameValue) ->
     io:fwrite("~B~n", [GameValue]);
 
 loop(N, GameValue) ->
     GameValue1 = negamax:negamax(-1, board:new()),
-    loop(N+1, max(GameValue, GameValue1)).
+    loop(N - 1, max(GameValue, GameValue1)).
 
-bench() ->
-    loop(0, -2).
+bench([RepString]) ->
+    {Reps, ""} = string:to_integer(RepString),
+    loop(Reps, -2).
--- erlang/ttt.sh
+++ erlang/ttt.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # http://erlangcentral.org/wiki/index.php?
 #  title=Running_Erlang_Code_From_The_Command_Line
-erl -pa . -run ttt -run init stop -noshell
+REPS=1
+if [ $# -gt 0 ]
+then
+    REPS="$1"
+fi
+erl -pa . -run ttt bench "$REPS" -run init stop -noshell
