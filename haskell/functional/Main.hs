999de7fc54a8919c8f04feb098fad76c0d0509b6
Bart Massey <bart@cs.pdx.edu>
2016-07-23 12:35:08 -0700

    updated haskell to take rep argument

--- haskell/functional/Main.hs
+++ haskell/functional/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 -- Copyright © 2015 Bart Massey
 -- [This program is licensed under the "MIT License"]
 -- Please see the file COPYING in the source
@@ -5,12 +6,19 @@
 
 -- Perfect Tic-Tac-Toe player in Haskell
 
+import Control.Monad
 import Data.Map
+import System.Environment
 
 import Negamax
 import GameValue (cells)
 
 main :: IO ()
 main = do
+  [repStr] <- getArgs
+  let reps = read repStr :: Int
   let board = fromAscList $ zip cells (repeat 0)
+  replicateM_ (reps - 1) $ do
+         let !_ = negamax 1 board
+         return ()
   print $ negamax 1 board
