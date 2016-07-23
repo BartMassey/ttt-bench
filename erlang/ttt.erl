fb952644cbcd6c9e2f798311dbc529ebde83189c
Bart Massey <bart@cs.pdx.edu>
2016-07-23 01:56:06 -0700

    added Erlang benchmark loop

--- erlang/ttt.erl
+++ erlang/ttt.erl
@@ -5,8 +5,18 @@
 % distribution of this software for license terms.
 
 -module(ttt).
--export([start/0]).
+-export([start/0, bench/0]).
 
 start() ->
     GameValue = negamax:negamax(-1, board:new()),
     io:fwrite("~B~n", [GameValue]).
+
+loop(10, GameValue) ->
+    io:fwrite("~B~n", [GameValue]);
+
+loop(N, GameValue) ->
+    GameValue1 = negamax:negamax(-1, board:new()),
+    loop(N+1, max(GameValue, GameValue1)).
+
+bench() ->
+    loop(0, -2).
