ffb440f32713292b4d068a4a638cfbaef167282b
Bart Massey <bart@cs.pdx.edu>
2016-07-23 12:35:08 -0700

    updated haskell to take rep argument

--- haskell/bobw/Main.hs
+++ haskell/bobw/Main.hs
@@ -5,12 +5,19 @@
 
 -- Perfect Tic-Tac-Toe player in Haskell
 
+import Control.Monad
 import Data.Array.IO
+import System.Environment
 
 import Negamax
 
 main :: IO ()
 main = do
+  [repStr] <- getArgs
+  let reps = read repStr :: Int
   board <- newArray ((0, 0), (2, 2)) 0 :: IO (IOUArray (Int, Int) Int)
+  replicateM_ (reps - 1) $ do
+         _ <- negamax 1 board
+         return ()
   result <- negamax 1 board
   print result
